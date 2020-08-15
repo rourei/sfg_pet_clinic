58fa34a697d7a0936be44ff52af1243c8ad5e66e
Rouven Reichert <rouven.reichert@stud.uni-hannover.de>
2020-08-15 14:32:54 +0200

    [L66] added vet index page and controller. Closes #6

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets(){
+        return "vets/index"; // make Thymeleaf look for a template in 'templates/vets' called '/index'
+    }
+}
