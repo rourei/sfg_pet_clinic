470c6ac72e2b88c9eeb7368708def7f3a52ff696
Rouven Reichert <rouven.reichert@stud.uni-hannover.de>
2020-08-15 14:45:55 +0200

    [L67] refactored using prefixing of request mapping

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -3,10 +3,11 @@ package guru.springframework.sfgpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/owners") // prefixing all following RequestMappings
 @Controller
 public class OwnerController {
 
-    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    @RequestMapping({"", "/", "/index", "/index.html"})
     public String listOwners(){
         return "owners/index"; // make Thymeleaf look for a template in 'templates/owners' called '/index'
     }
