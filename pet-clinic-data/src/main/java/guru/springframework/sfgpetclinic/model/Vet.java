48d73c119fff1dee8004256001818637c20a165a
Rouven Reichert <rouven.reichert@stud.uni-hannover.de>
2020-08-16 18:56:13 +0200

    [REFACTOR] added initialization for sets to avoid NPE at runtime

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,10 +1,11 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Vet extends Person {
 
-    private Set<Specialty> specialties;
+    private Set<Specialty> specialties = new HashSet<>(); // initialize as empty set to avoid NPE on first access
 
     public Set<Specialty> getSpecialties() {
         return specialties;
