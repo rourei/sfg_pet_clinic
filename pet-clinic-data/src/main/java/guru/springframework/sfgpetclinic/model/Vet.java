ef00aaeeffbb5f8e100c86fd986e262a634a0a0f
Rouven Reichert <rouven.reichert@stud.uni-hannover.de>
2020-08-17 16:47:30 +0200

    [L147] converted Vet and specialty to JPA entities. Closes #36.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,10 +1,16 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER) // following the official implementation
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "specialty"))
     private Set<Specialty> specialties = new HashSet<>(); // initialize as empty set to avoid NPE on first access
 
     public Set<Specialty> getSpecialties() {
