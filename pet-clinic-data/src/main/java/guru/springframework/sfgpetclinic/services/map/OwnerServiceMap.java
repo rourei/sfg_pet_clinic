f0bf9ee735a2643a87bf9b25f9e46c7e5d032fe2
Rouven Reichert <rouven.reichert@stud.uni-hannover.de>
2020-08-17 06:53:08 +0200

    [L123] refactored save method to ensure consistency with database

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -1,7 +1,10 @@
 package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.PetService;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
@@ -10,6 +13,15 @@ import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    // Using Spring's Dependency Injection
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
 
     @Override
     public Set<Owner> findAll() {
@@ -23,7 +35,41 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner object) {
-        return super.save(object);
+
+        // This save-method also checks if all properties of the assigned pets are set.
+        // If not, it uses the injected services to assign the missing properties from within the database.
+
+        if (object != null) {
+            // Check if owner has any pets
+            if (object.getPets() != null) {
+                // Iterate over all existing pets
+                object.getPets().forEach(pet -> {
+                    // Check if PetType is specified , if not -> runtime exception
+                    if (pet.getPetType() != null){
+                        // Check if ID property is already set -> ensure consistency with already saved pet types
+                        if (pet.getPetType().getId() == null) {
+                            // Use injected petTypeService to assign ID if not specified yet
+                            pet.setPetType(petTypeService.save(pet.getPetType()));
+                        }
+                    } else {
+                        throw new RuntimeException("Pet Type is required.");
+                    }
+
+                    // Check if pet ID is already specified
+                    if (pet.getId() == null) {
+                        // If not, use injected petService to assign it
+                        Pet savedPet = petService.save(pet);
+                        // Use the saved pet (carrying an ID) to set the pet ID -> consistency
+                        pet.setId(savedPet.getId());
+                    }
+                });
+            }
+            // Call method of the parent class
+            return super.save(object);
+
+        } else {
+            return null;
+        }
     }
 
     @Override
