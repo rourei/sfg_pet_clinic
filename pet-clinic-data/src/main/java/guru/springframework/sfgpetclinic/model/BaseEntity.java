a084da5f490a7fb3c19bf0d83cad99107ac3201c
Rouven Reichert <rouven.reichert@stud.uni-hannover.de>
2020-08-17 16:16:27 +0200

    [L145] annotated base entity. Closes #34.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,9 +1,16 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass // tells JPA that this is a base class that others inherit from. Does not need to be mapped to the database
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // retrieve ID from database, ties to specific database (MySQL here)
     private Long id; // Hibernate recommends box types over primitives (Long instead of long)
 
     public Long getId() {
