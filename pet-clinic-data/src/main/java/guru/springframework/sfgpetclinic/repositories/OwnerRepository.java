69e2dac9c00a06225d8970abf6e1a3949cdcba67
Rouven Reichert <rouven.reichert@stud.uni-hannover.de>
2020-08-18 09:48:27 +0200

    [150] added JPA service for owner. Closes #39.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -3,11 +3,19 @@ package guru.springframework.sfgpetclinic.repositories;
 import guru.springframework.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Set;
+
 // Arguments for CRUDRepository are object type (Owner) and ID value type (Long)
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     // The implementation of these repositories is provided by Spring at runtime to provide persistence and
     // query operations.
     // Under the hood, this repository takes care of Hibernate related commands, transactions, etc.
+
+    Owner findByLastName(String lastName);
+
+    // The original findAll() method from the CrudRepository returns an Iterable. Since the Map-based implementation
+    // returns a Set of Owners we need to 'convert' that return value into a set.
+    Set<Owner> findAll();
 
 }
