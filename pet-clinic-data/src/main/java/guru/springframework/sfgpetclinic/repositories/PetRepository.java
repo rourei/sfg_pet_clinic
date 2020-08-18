7f3cb2ee7b3f7b0c3a3ce7e714c6caa1907f61b6
Rouven Reichert <rouven.reichert@stud.uni-hannover.de>
2020-08-18 10:40:12 +0200

    [L153] added JPA service for pet. Closes #42.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/PetRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/PetRepository.java
@@ -3,11 +3,17 @@ package guru.springframework.sfgpetclinic.repositories;
 import guru.springframework.sfgpetclinic.model.Pet;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Set;
+
 // Arguments for CRUDRepository are object type (Pet) and ID value type (Long)
 public interface PetRepository extends CrudRepository<Pet, Long> {
 
     // The implementation of these repositories is provided by Spring at runtime to provide persistence and
     // query operations.
     // Under the hood, this repository takes care of Hibernate related commands, transactions, etc.
+
+    // The original findAll() method from the CrudRepository returns an Iterable. Since the Map-based implementation
+    // returns a Set of Pets we need to 'convert' that return value into a set.
+    Set<Pet> findAll();
 
 }
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,54 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.repositories.PetRepository;
+import guru.springframework.sfgpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetSDJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    // Use Spring's dependency Injection
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+
+        // Initialize return value as empty set to avoid NPE at runtime
+        Set<Pet> pets = new HashSet<>();
+        // Iterate over all available pets and add each pet to the return value using a method reference
+        petRepository.findAll().forEach(pets::add);
+
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        // Return value from Optional if available or else return null
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
